50885221ff593e9203f03d87617daaf89ba7857b
KitkatKayla <kitkatkayla0216@gmail.com>
2020-02-08 20:06:18 +0000

    text changes

--- commands/general/info.js
+++ commands/general/info.js
@@ -6,6 +6,9 @@ module.exports.run = async (bot, message, args) => {
 
     message.author.send({embed: {
         color: 16754418,
+        thumbnail: {
+            url: `https://i.pinimg.com/originals/c5/38/df/c538df94fc55387873b7d8aed4499786.jpg` // Image is placeholder
+        },
         author: {
             name: bot.user.username,
             icon_url: bot.user.avatarURL
@@ -15,8 +18,10 @@ module.exports.run = async (bot, message, args) => {
         description: 
     (`Guardian Angel is a custom bot, being designed to serve this server and possibly others in the future. I am being programmed to hopefully advance far enough to take priority over most of the other popular bots.
 
+    • My prefix is \`${config.prefix}\`. The option to change this is planned.
     • Type \`&help <command>\` for more info on that command!
-    • DM \`Snowflake#1001\` for any bot idea submissions\n •Find out how you can help support the bot in ${message.guild.channels.get('668523943682113606').toString()}\n •My prefix is \`${config.prefix}\`. The option to change this is planned.
+    • DM \`Snowflake#1001\` for any bot idea submissions
+    • Find out ways to help support me in ${message.guild.channels.get('668523943682113606').toString()}\n
         
     **Command Information**
     Basic \`user\` command information:
